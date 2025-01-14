bad3499ea48cf4e00a8c10b41dbfa943a738de54
Darkblaze30 <juanchiquiza30@gmail.com>
2025-01-14 00:27:50 -0500

    feat: :sparkles: intento review

---
+++ funciones, numbers y string/review/ejemplo.py
@@ -0,0 +1,39 @@
+print(""""
+      ==============================================
+      ======= Welcome to picas and fijas game ======
+      ==============================================
+      """)
+
+NUMERO = "9410"
+numeroEncontrado = False
+numeroEvaluar = ""
+
+while not numeroEncontrado:
+    numContrincante = input("Ingrese el numero a evaluar: ")
+    numeros_repetidos = False
+    for num in numContrincante:
+        if numContrincante.count(num) > 1:
+            numeros_repetidos = True
+    if numeros_repetidos:
+        print("No se pueden ingresar numeros repetidos")
+    else:
+        picas = 0
+        fijas = 0
+
+        if NUMERO == numContrincante:
+            numeroEncontrado = True
+        else:
+            #Buscar las fijas
+            for i in range(0, 4):
+                x = NUMERO.find(numContrincante[i])
+                if x == i:
+                    fijas += 1
+                elif x != -1:
+                    picas += 1
+            print(f"Tiene {picas} picas y {fijas} fijas.")
+
+        #para arreglar
+        #numeroEncontrado = True
+
+    if numeroEncontrado:
+        print( "Has encontrado el numero!")
--- funciones, numbers y string/review/main.js
+++ funciones, numbers y string/review/main.js
@@ -2,7 +2,20 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-console.log(getRandomInt(99999));
+let numero = String(getRandomInt(20))
+do{
+  numero = String(getRandomInt(99999))
+  for (let i = 0; i < numero.length; i++) {
+    const element = numero[i];
+    element
+  }
+  console.log("es menor")
+  console.log(numero)
+  console.log(numero.length)
+
+}while(numero.length < 5 || repetido === true)
+
+console.log(numero);
   // Expected output: 0, 1 or 2
 
 let picas = 0
@@ -12,3 +25,19 @@ do{
     break
 }while(true)
 
+const generatedNumbers = new Set();
+
+// Función para generar un número aleatorio de 5 cifras
+function generateRandomNumber() {
+  if (generatedNumbers.size === 90000) {
+    return "Se han generado todos los números posibles de 5 cifras.";
+  }
+
+  let number;
+  do {
+    number = Math.floor(Math.random() * 90000) + 10000; // Números entre 10000 y 99999
+  } while (generatedNumbers.has(number));
+
+  generatedNumbers.add(number);
+  return number;
+}
